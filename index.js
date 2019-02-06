662dd3e39646ed9f076f5dbfc238160d9c5fa81c
scott <wscarl@crimson.ua.edu>
2019-02-05 18:09:41 -0600

    Installed Mongoose. Set up initial connection to mongodb on a separate local machine with temporary dev user

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+mongoose.connect('mongodb://local-dev:supersecret@10.0.0.22:27017/test').then(
+	() => { console.log('Connected to database') },
+	err => { console.log(`Error occurred connecting to database: ${err}`) }
+);
 
 app.get('/', (req, res) => res.send('Hello world!'))
 
